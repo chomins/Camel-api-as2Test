8a448fc7e1f09247acf78ef7f722ded331c17bbc
조민수(학부생-소프트웨어전공) <cms4317@kookmin.kr>
2021-09-17 13:20:31 +0900

    first

---
+++ src/main/java/com/example/camelas2api/CamelAs2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.camelas2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelAs2ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelAs2ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/camelas2api/CamelAs2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelas2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelAs2ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
